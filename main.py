a0a45620d247e10d7c2dd71d7cb45b2b2f66b3cb
sy-robot-factory <101233013+sy-robot-factory@users.noreply.github.com>
2025-10-15 11:35:04 +0900

    Betty用デモ作成

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.modules.pm_servo_control_hiwonder_serial_bus_servo_controller import PM
 from src.test.pm_test_shiro import PMTestShiro
 from src.test.motion_lang_test_shiro import MotionLangTestShiro
 from src.test.pm_demo_shiro import PMDemoShiro
+from src.test.pm_demo_betty import PMDemoBetty
 
 
 def main():
@@ -16,6 +17,9 @@ def main():
     print("SOUR: Simple Operation for Ubiquitous Robotics")
 
     robot_properties = RobotProperties()
+
+    ## properties for shiro
+    """
     robot_properties.num_servos = 14
     robot_properties.servo_ids = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     robot_properties.servo_min_positions = [100.0, 0.0, 500.0, 450.0, 100.0, 130.0, 150.0, 100.0, 90.0, 200.0, 400.0, 100.0, 450.0, 150.0]
@@ -23,7 +27,14 @@ def main():
     #robot_properties.servo_initial_positions = [500.0, 350.0, 450.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 370.0, 600.0, 500.0, 700.0, 300.0]
     robot_properties.servo_initial_positions = [500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0, 500.0]
     robot_properties.servo_shifts = [0.0, -150.0, -50.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 130.0, -300.0, 0.0, 200.0, -200.0]
-
+    """
+    ## properties for betty
+    robot_properties.num_servos = 4
+    robot_properties.servo_ids = [0, 1, 2, 3]
+    robot_properties.servo_min_positions = [500.0, 500.0, 500.0, 500.0]
+    robot_properties.servo_max_positions = [2500.0, 2500.0, 2500.0, 2500.0]
+    robot_properties.servo_initial_positions = [1500.0, 1500.0, 1500.0, 1500.0]
+    robot_properties.servo_shifts = [0.0, 0.0, 0.0, 0.0]
     robot_properties.servo_easing_function = ServoEasingFunctions.EASE_IN_OUT_CUBIC
 
 
@@ -31,8 +42,9 @@ def main():
 
     #register periodic modules
     #r_core.register_module(PMTestShiro(robot_properties, 3000))
-    # r_core.register_module(MotionLangTestShiro(robot_properties, 3000)) gMLPで動作させるとき
-    r_core.register_module(PMDemoShiro(robot_properties, 100))
+    #r_core.register_module(MotionLangTestShiro(robot_properties, 3000)) gMLPで動作させるとき
+    #r_core.register_module(PMDemoShiro(robot_properties, 100)) #demo for shiro
+    r_core.register_module(PMDemoBetty(robot_properties, 100))
 
     if debug_mode:
         r_core.register_module(PMServoControl(robot_properties, 100))
---
+++ src/test/pm_demo_betty.py
@@ -0,0 +1,72 @@
+from src.core.periodic_module import PeriodicModule
+from src.test.motion import Motion
+
+import random
+import copy
+
+class BettyDemoMotionData():
+
+    ## motion for 4 DoF
+    demo_motion_default_position = [
+        [1500.0, 1500.0, 1500.0, 1500.0]
+    ]
+
+    demo_motion_default_time = [
+        [1000.0, 1000.0, 1000.0, 1000.0]
+    ]
+
+    demo_motion_default_interval = [
+        [1000.0]
+    ]
+
+
+    demo_motion_raise_hands_position = [
+        [1500.0, 1500.0, 1500.0, 1500.0]
+    ]
+
+    demo_motion_raise_hands_time = [
+        [1000.0, 1000.0, 1000.0, 1000.0]
+    ]
+
+    demo_motion_raise_hands_interval = [
+        [1000.0]
+    ]
+
+    def __init__(self):
+        pass
+
+    def get_demo_motion_default(self):
+        return Motion(self.demo_motion_default_position, self.demo_motion_default_time, self.demo_motion_default_interval, loop_time=0, fluc_type=Motion.FLUCTUATION_TYPE_UNIFORM, fluc_amp_motion=50, fluc_amp_time=100, fluc_amp_interval=1000)
+
+    def get_demo_motion_raise_hands(self):
+        return Motion(self.demo_motion_raise_hands_position, self.demo_motion_raise_hands_time, self.demo_motion_raise_hands_interval, loop_time=1, fluc_type=Motion.FLUCTUATION_TYPE_UNIFORM, fluc_amp_motion=50, fluc_amp_time=100, fluc_amp_interval=1000)
+
+    class PMDemoBetty(PeriodicModule):
+        def __init__(self, robot_properties, interval_ms=1000):
+            super().__init__(interval_ms)
+            self.robot_properties = robot_properties
+            self.demo_motion_default = BettyDemoMotionData().get_demo_motion_default()
+            self.demo_motion_raise_hands = BettyDemoMotionData().get_demo_motion_raise_hands()
+            self.next_motion_interval = 0
+            self.current_motion = self.demo_motion_default
+
+        def execute_periodic_task(self, lock, data_dict):
+
+            super().execute_periodic_task(lock, data_dict)
+
+            if data_dict['servo_ready'] == True and data_dict['servo_params_updated'] == False:
+
+                if self.next_motion_interval > 0:#interval time
+                    self.next_motion_interval -= self.interval_ms
+                else:
+                    if not self.current_motion.is_motion_finished():
+                        position, time, interval = self.current_motion.get_next_motion()
+
+                        self.write_servo_positions(lock, data_dict, position, time)
+
+                        self.next_motion_interval = interval
+
+                    else:
+                        # state transition
+                        pass
+
