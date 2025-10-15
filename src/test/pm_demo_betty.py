e0b89600f2b4e87d10de493ac218fff2ce3403e8
sy-robot-factory <101233013+sy-robot-factory@users.noreply.github.com>
2025-10-15 12:06:04 +0900

    インデント修正

--- src/test/pm_demo_betty.py
+++ src/test/pm_demo_betty.py
@@ -41,32 +41,32 @@ class BettyDemoMotionData():
     def get_demo_motion_raise_hands(self):
         return Motion(self.demo_motion_raise_hands_position, self.demo_motion_raise_hands_time, self.demo_motion_raise_hands_interval, loop_time=1, fluc_type=Motion.FLUCTUATION_TYPE_UNIFORM, fluc_amp_motion=50, fluc_amp_time=100, fluc_amp_interval=1000)
 
-    class PMDemoBetty(PeriodicModule):
-        def __init__(self, robot_properties, interval_ms=1000):
-            super().__init__(interval_ms)
-            self.robot_properties = robot_properties
-            self.demo_motion_default = BettyDemoMotionData().get_demo_motion_default()
-            self.demo_motion_raise_hands = BettyDemoMotionData().get_demo_motion_raise_hands()
-            self.next_motion_interval = 0
-            self.current_motion = self.demo_motion_default
+class PMDemoBetty(PeriodicModule):
+    def __init__(self, robot_properties, interval_ms=1000):
+        super().__init__(interval_ms)
+        self.robot_properties = robot_properties
+        self.demo_motion_default = BettyDemoMotionData().get_demo_motion_default()
+        self.demo_motion_raise_hands = BettyDemoMotionData().get_demo_motion_raise_hands()
+        self.next_motion_interval = 0
+        self.current_motion = self.demo_motion_default
 
-        def execute_periodic_task(self, lock, data_dict):
+    def execute_periodic_task(self, lock, data_dict):
 
-            super().execute_periodic_task(lock, data_dict)
+        super().execute_periodic_task(lock, data_dict)
 
-            if data_dict['servo_ready'] == True and data_dict['servo_params_updated'] == False:
+        if data_dict['servo_ready'] == True and data_dict['servo_params_updated'] == False:
 
-                if self.next_motion_interval > 0:#interval time
-                    self.next_motion_interval -= self.interval_ms
+            if self.next_motion_interval > 0:#interval time
+                self.next_motion_interval -= self.interval_ms
+            else:
+                if not self.current_motion.is_motion_finished():
+                    position, time, interval = self.current_motion.get_next_motion()
+
+                    self.write_servo_positions(lock, data_dict, position, time)
+
+                    self.next_motion_interval = interval
+
                 else:
-                    if not self.current_motion.is_motion_finished():
-                        position, time, interval = self.current_motion.get_next_motion()
-
-                        self.write_servo_positions(lock, data_dict, position, time)
-
-                        self.next_motion_interval = interval
-
-                    else:
-                        # state transition
-                        pass
+                    # state transition
+                    pass
 
