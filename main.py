cbd79167962ed5fc222c1e4d8f0ff5b57a4ea094
sy-robot-factory <101233013+sy-robot-factory@users.noreply.github.com>
2025-11-12 13:48:20 +0900

    mpu 6886 test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.core.robot_properties import RobotProperties, ServoEasingFunctions
 from src.modules.pm_servo_control import PMServoControl
 from src.modules.pm_servo_control_hiwonder_serial_bus_servo_controller import PMServoControlHiwonderSerialBusServoController
 from src.modules.pm_unitv2 import PMUnitV2
+from src.modules.pm_mpu6886_imu import PMMPU6886IMU
 
 from src.test.pm_test_shiro import PMTestShiro
 from src.test.motion_lang_test_shiro import MotionLangTestShiro
@@ -30,6 +31,7 @@ def main():
     robot_properties.servo_shifts = [0.0, -150.0, -50.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 130.0, -300.0, 0.0, 200.0, -200.0]
     """
     ## properties for betty
+    """
     robot_properties.num_servos = 4
     robot_properties.servo_ids = [0, 1, 2, 3]
     robot_properties.servo_min_positions = [500.0, 500.0, 500.0, 500.0]
@@ -39,7 +41,7 @@ def main():
     robot_properties.servo_easing_function = ServoEasingFunctions.EASE_IN_OUT_CUBIC
     robot_properties.servo_controller_port = '/dev/ttyAMA2'
     robot_properties.camera_port = '/dev/ttyAMA4'
-
+    """
 
     r_core = RobotCore(robot_properties)
 
@@ -47,9 +49,11 @@ def main():
     #r_core.register_module(PMTestShiro(robot_properties, 3000))
     #r_core.register_module(MotionLangTestShiro(robot_properties, 3000)) gMLPで動作させるとき
     #r_core.register_module(PMDemoShiro(robot_properties, 100)) #demo for shiro
-    r_core.register_module(PMDemoBetty(robot_properties, 80))
+    r_core.register_module(PMMPU6886IMU(robot_properties, 100))
 
-    r_core.register_module(PMUnitV2(robot_properties, 500))
+    ## for betty
+    #r_core.register_module(PMDemoBetty(robot_properties, 80))
+    #r_core.register_module(PMUnitV2(robot_properties, 500))
 
     if debug_mode:
         r_core.register_module(PMServoControl(robot_properties, 100))
---
+++ src/modules/pm_mpu6886_imu.py
@@ -0,0 +1,28 @@
+from src.core.periodic_module import PeriodicModule
+from src.communication.mpu6886 import MPU6886
+
+class PMMPU6886IMU(PeriodicModule):
+    def __init__(self, interval_ms=1000):
+        super().__init__(interval_ms)
+        self.mpu6886 = MPU6886()
+        self.mpu6886.initialize()
+
+
+    def execute_periodic_task(self, lock, data_dict):
+        super().execute_periodic_task(lock, data_dict)
+
+        gx, gy, gz = self.mpu6886.get_gyro_data()
+        ax, ay, az = self.mpu6886.get_accel_data()
+        temp = self.mpu6886.get_temp_data()
+
+        print(f"gx: {gx}, gy: {gy}, gz: {gz}, ax: {ax}, ay: {ay}, az: {az}, temp: {temp}")
+
+        #data_dict['gx'] = gx
+        #data_dict['gy'] = gy
+        #data_dict['gz'] = gz
+
+        #data_dict['ax'] = ax
+        #data_dict['ay'] = ay
+        #data_dict['az'] = az
+
+        #data_dict['temp'] = temp
